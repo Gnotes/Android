cfe22a74cd861ccdd40060ec197605a75411a6d7
xing.he <1245036094@qq.com>
2018-03-05 20:31:26 +0800

    example: 添加页面跳转示例，Intent简单用法

--- example/FirstActivity/app/src/main/java/com/example/xing/firstactivity/MainActivity.java
+++ example/FirstActivity/app/src/main/java/com/example/xing/firstactivity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.xing.firstactivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -20,6 +22,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"I'm a Button ,you just clicked me!",Toast.LENGTH_LONG).show();
+                // Intent对象有过个构造
+                // 创建Intent对象，第一个参数是启动活动的上下文环境，第二个参数指定想要启动的目标活动
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                // 通过startActivity启动活动
+                startActivity(intent);
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent的另一个构造，通过指定AndroidManifest.xml中activity里边的intent-filter action属性，来匹配要启动的活动
+                startActivity(new Intent("com.example.xing.firstactivity.ACTION_START"));
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button_4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.xing.firstactivity.ACTION_START");
+                // 指定分类category,没有定义该category，点击会导致程序崩溃！
+                intent.addCategory("com.example.xing.firstactivity.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+
+        Button button5 = (Button) findViewById(R.id.button_5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.xing.firstactivity.ACTION_START");
+                intent.addCategory("com.example.xing.firstactivity.SECOND_CATEGORY");
+                startActivity(intent);
+            }
+        });
+
+        Button button6 = (Button) findViewById(R.id.button_6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/"));
+                startActivity(intent);
             }
         });
     }
---
+++ example/README.md
@@ -0,0 +1,8 @@
+# Example
+
+学习实例目录
+
+## CONTENTS
+
+- [HelloWorld](./HelloWorld)
+- [FistActivity](./FistActivity)
